a8fe743d7675af68ec3b28523a854d44ed02205e
Kyxtaka <randriantsoanathan1@gmail.com>
2024-02-22 12:25:29 +0100

    fix Salle

--- Salle.java
+++
@@ -1,9 +0,0 @@
-public class Salle {
-    private String name;
-    private Horaire uneHoraire;
-
-    public Salle(String name, Horaire uneHoraire){
-        this.name = name;
-        this.uneHoraire = uneHoraire;
-    }
-}
