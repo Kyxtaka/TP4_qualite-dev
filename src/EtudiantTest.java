5eb84bd72118b4039e656c06cc6c66bd2e054d09
Kyxtaka <randriantsoanathan1@gmail.com>
2024-02-22 11:11:22 +0100

    Implementation de Etudiant + Test avec Junit de nos nom

---
+++ src/EtudiantTest.java
@@ -0,0 +1,18 @@
+
+import org.junit.*;
+import static org.junit.Assert.assertEquals;
+
+public class EtudiantTest {
+    @Test
+    public void testEtuciantCreate1() {
+        Etudiant etu1 = new Etudiant("Nathan","Randriantsoa");
+        assertEquals(etu1.getFirstName(), "Nathan");
+        assertEquals(etu1.getLastName(), "Randriantsoa");
+    }
+    @Test
+    public void testEtuciantCreate2() {
+        Etudiant etu2 = new Etudiant("Joann","Raignault");
+        assertEquals(etu2.getFirstName(), "Joann");
+        assertEquals(etu2.getLastName(), "Raignault");
+    }
+}
