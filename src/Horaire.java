5ad475ab4a6a108340df15a2e35b0dcc8b22424e
Nathan Randriantsoa <134612654+Kyxtaka@users.noreply.github.com>
2024-02-22 12:19:46 +0100

    Horaire (#8)
    * création horaire.java (rien dans le fichier)
    
    * Readme
    
    * début classe horaire (il faudrait créer startDate et endDate)
    
    * Delete src directory
    
    * Horaire.java (attributs)
    
    * Delete README.md
    
    * création horaire.java (rien dans le fichier)
    
    * début classe horaire (il faudrait créer startDate et endDate)
    
    * Horaire.java (attributs)
    
    * Delete README.md
    
    * fix de Horaire dans src
    
    * Fix de Horaire
    
    ---------
    
    Co-authored-by: Joann45 <joann.raignault@gmail.com>
    Co-authored-by: Joann45 <143809232+Joann45@users.noreply.github.com>

---
+++ src/Horaire.java
@@ -0,0 +1,14 @@
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.List;
+
+public class Horaire {
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private List<Salle> lesSalles;
+    private List<Cours> lesCours;
+
+    public Horaire(){
+
+    }
+}
