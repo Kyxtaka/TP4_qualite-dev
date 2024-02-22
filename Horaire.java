25749af2afc860a70291caec57a2b9f5dcc8bbdd
Joann45 <joann.raignault@gmail.com>
2024-02-22 11:26:25 +0100

    Horaire.java (attributs)

--- Horaire.java
+++ Horaire.java
@@ -3,9 +3,12 @@ import java.time.Month;
 import java.util.List;
 
 public class Horaire {
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
     private List<Salle> lesSalles;
     private List<Cours> lesCours;
 
     public Horaire(){
+
     }
 }
