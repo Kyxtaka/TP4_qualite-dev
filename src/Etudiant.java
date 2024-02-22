ac9261657f357a24f51aa1eb84859d0cce4fb0a0
Nathan Randriantsoa <134612654+Kyxtaka@users.noreply.github.com>
2024-02-22 11:15:01 +0100

    Etudiant (#1)
    * Etudiant initialisation class
    
    * Implementation de Etudiant + Test avec Junit de nos nom

---
+++ src/Etudiant.java
@@ -0,0 +1,15 @@
+public class Etudiant {
+    private String firstName;
+    private String lastName;
+
+    public Etudiant(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+        public String getFirstName() {
+            return this.firstName;
+        }
+        public String getLastName() {
+            return this.lastName;
+        }
+}
