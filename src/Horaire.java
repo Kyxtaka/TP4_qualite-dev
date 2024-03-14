da111934a3a49045379a9213ba256e63578e37f0
Joann45 <joann.raignault@gmail.com>
2024-03-14 09:09:24 +0100

    Ajout de Horaire

--- src/Horaire.java
+++ src/Horaire.java
@@ -1,14 +1,18 @@
 import java.time.LocalDateTime;
 import java.time.Month;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Horaire {
-    private LocalDateTime startDate;
-    private LocalDateTime endDate;
+    private String startDate;
+    private String endDate;
     private List<Salle> lesSalles;
     private List<Cours> lesCours;
 
-    public Horaire(){
-
+    public Horaire(String startDate, String endDate){
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.lesSalles = new ArrayList<>();
+        this.lesCours = new ArrayList<>();
     }
 }
